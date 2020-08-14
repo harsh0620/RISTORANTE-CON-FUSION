428ecf5406f499eb944733b881a8e867fab4957d
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2020-08-14 19:35:05 +0530

    react-router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Menu from "./MenuComponents";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { DISHES } from "../shared/dishes";
 import DishDetail from "./DishDetailComponent";
 
@@ -10,27 +12,27 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
     };
   }
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
+
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
+
     return (
       <div>
         <Header />
-        <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        />
-        <DishDetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home" />
+        </Switch>
+
         <Footer />
       </div>
     );
